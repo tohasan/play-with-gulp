25dbc0d346dcb670b1fbbcc87b609970376f7350
tohasan <tohasan@yandex.ru>
2016-10-22 10:52:35 +0300

    some types of gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,34 @@ gulp.task('hello', function (callback) {
     // Signal async completion
     callback();
 });
+
+gulp.task('example:promise', function () {
+    return new Promise((resolve) => {
+        // ...
+        resolve('result');
+    });
+});
+
+gulp.task('example:stream', function () {
+    // Read all from steam (and throw the data away) and then done
+    return require('fs').createReadStream(__filename);
+});
+
+gulp.task('example:process', function () {
+    // Returns child process
+    return require('child_process').spawn('ping', ['127.0.0.1']);
+});
+
+gulp.task('example', gulp.series(
+    'hello',
+    'example:promise',
+    'example:stream',
+    'example:process'
+));
+
+gulp.task('example:parallel', gulp.parallel(
+    'hello',
+    'example:promise',
+    'example:stream',
+    'example:process'
+));
