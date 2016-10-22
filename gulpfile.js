7a9e829eafbaf61a6338ff62bc178ebf30f936bd
tohasan <tohasan@yandex.ru>
2016-10-22 14:23:09 +0300

    build stylus styles

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,16 @@
 'use strict'; // NOSONAR
 
 const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const concat = require('gulp-concat');
+const debug = require('gulp-debug');
 
-gulp.task('default', function () {
-    // Patterns to search files are from minimatch library
-    return gulp.src('src/**/*.*', {
-        // Use this option if you don't want to read content of files
-        // read: false
-    })
-        .on('data', function (file) {
-            console.log({ // NOSONAR
-                contents: file.contents, //  <Buffer 2f 2a 2a 0d 0a 20 2a 20 43 72 65 61 74 65 64 20 77 69 74 68 20 49 6e 74 65 6c 6c 69 4a 20 49 44 45 41 20 28 69 6e 20 61 63 63 6f 72 64 61 6e 63 65 20 ... >
-                cwd: file.cwd, // E:\\projects\\experiments\\play-with-gulp
-                base: file.base, // E:\\projects\\experiments\\play-with-gulp\\src\\
-                path: file.path,  // E:\\projects\\experiments\\play-with-gulp\\src\\js\\libs\\somelib.js
-                // Path component helpers
-                relative: file.relative, // js\\libs\\somelib.js
-                dirname: file.dirname, // E:\\projects\\experiments\\play-with-gulp\\src\\js\\libs
-                basename: file.basename, // somelib.js
-                stem: file.stem, // somelib
-                extname: file.extname // .js
-            });
-        })
-        .pipe(gulp.dest(function (file) {
-            let relative = 'other';
-            if (file.extname === '.js') {
-                relative = 'outjs';
-            } else if (file.extname === '.css') {
-                relative = 'outcss';
-            }
-            return `dest/${relative}`;
-        }));
+gulp.task('styles', function () {
+    return gulp.src('src/styles/styles.styl')
+        .pipe(debug({ title: 'src' }))
+        .pipe(stylus())
+        .pipe(debug({ title: 'stylus' }))
+        .pipe(gulp.dest('public'));
 });
+
+gulp.task('default', gulp.series('styles'));
